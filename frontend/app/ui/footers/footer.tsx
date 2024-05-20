4162b33d6330b2fcdc64cc3e68b3c24b10eb3e03
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-05-20 18:11:00 +0200

    make footer fixed on large devices and add hover state for links

--- frontend/app/ui/footers/footer.tsx
+++ frontend/app/ui/footers/footer.tsx
@@ -1,15 +1,21 @@
 export default function Footer() {
   return (
-    <footer className="relative z-50">
+    <footer className="relative z-50 lg:fixed bottom-0 right-0">
       <ul className="flex gap-3 text-shark-600 p-5 mt-4 justify-end">
         <li>
-          <a className="hover:text-shark-950 hover:underline">email</a>
+          <a className="hover:text-shark-950 hover:underline hover:cursor-pointer">
+            email
+          </a>
         </li>
         <li>
-          <a className="hover:text-shark-950 hover:underline">github</a>
+          <a className="hover:text-shark-950 hover:underline hover:cursor-pointer">
+            github
+          </a>
         </li>
         <li>
-          <a className="hover:text-shark-950 hover:underline">instagram</a>
+          <a className="hover:text-shark-950 hover:underline hover:cursor-pointer">
+            instagram
+          </a>
         </li>
       </ul>
     </footer>
