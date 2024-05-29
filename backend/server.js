e637a2db058372e08156d75362cf90d7b6659a39
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-05-29 10:18:19 +0200

    fix cors policy

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "localhost:3000", // Be cautious in production, use specific domains instead of '*'
+    origin: "http://localhost:3000", // Be cautious in production, use specific domains instead of '*'
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow credentials (cookies)
