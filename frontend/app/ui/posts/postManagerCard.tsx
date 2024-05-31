82317002913dcee36dd24c728a280aa9a9ed3620
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-05-31 18:26:45 +0200

    support dark theme for post manager card

--- frontend/app/ui/posts/postManagerCard.tsx
+++ frontend/app/ui/posts/postManagerCard.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function PostManagerCard({ post }: { post: PostType }) {
   return (
-    <div className="w-full bg-blue-100 p-4 flex flex-col gap-4 rounded-md">
+    <div className="w-full bg-blue-100 dark:bg-slate-600 p-4 flex flex-col gap-4 rounded-md">
       <h1>{post.title}</h1>
       <div className="flex gap-4">
         <Link
-          className="bg-yellow-300 hover:bg-yellow-400 px-4 py-1.5 rounded-md"
+          className="bg-yellow-300 hover:bg-yellow-400 dark:text-black px-4 py-1.5 rounded-md"
           href="#"
         >
           Edit
